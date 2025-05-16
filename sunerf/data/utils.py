77147f612bc2569f453d319e37f35b54ccda9a59
RobertJaro <robert.jarolim@gmail.com>
2025-05-16 17:30:44 -0600

    fix solar center shifts of get_azimuthal_equidistant_coordinates

--- sunerf/data/utils.py
+++ sunerf/data/utils.py
@@ -24,19 +24,25 @@ so_norms = {304: ImageNormalize(vmin=0, vmax=300, stretch=LinearStretch(), clip=
 
 sdo_cmaps = {171: cm.sdoaia171, 174: cm.sdoaia171, 193: cm.sdoaia193, 211: cm.sdoaia211, 304: cm.sdoaia304}
 
+
 def get_azimuthal_equidistant_coordinates(s_map):
     data = s_map.data
+
     coord_grid = np.mgrid[0:data.shape[0], 0:data.shape[1]]
     coord_grid = np.stack(coord_grid, dtype=np.float32).T
     coord_grid[..., 0] = coord_grid[..., 0] - (data.shape[0] - 1) / 2
     coord_grid[..., 1] = coord_grid[..., 1] - (data.shape[1] - 1) / 2
 
-    coord_grid[..., 0] = coord_grid[..., 0] * s_map.scale[0].to_value(u.arcsec/ u.pix)
-    coord_grid[..., 1] = coord_grid[..., 1] * s_map.scale[1].to_value(u.arcsec/ u.pix)
+    coord_grid[..., 0] = coord_grid[..., 0] * s_map.scale[0].to_value(u.arcsec / u.pix)
+    coord_grid[..., 1] = coord_grid[..., 1] * s_map.scale[1].to_value(u.arcsec / u.pix)
 
     coord_grid = coord_grid * u.arcsec
-    return coord_grid
 
+    # shift center
+    coord_grid[..., 0] = coord_grid[..., 0] + s_map.reference_coordinate.Tx.to(u.arcsec)
+    coord_grid[..., 1] = coord_grid[..., 1] + s_map.reference_coordinate.Ty.to(u.arcsec)
+
+    return coord_grid
 
 
 def loadAIAMap(file_path, resolution=1024, map_reproject=False):
